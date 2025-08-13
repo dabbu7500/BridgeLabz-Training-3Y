b9713ba78f7a48990dcfca16163d357f001277ae
Himanshu Choudhary <chaudharyhimanshu276@gmail.com>
2025-08-13 12:23:11 +0530

    add problem2

---
+++ fundamental-prectice-problem/Add_Two_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Add_Two_Numbers {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("enter first No. :");
+        int firstNumber=sc.nextInt();
+        System.out.print("enter second No. :");
+        int secondNumber=sc.nextInt();
+        System.out.println("sum ="+(firstNumber+secondNumber));
+
+    }
+}
