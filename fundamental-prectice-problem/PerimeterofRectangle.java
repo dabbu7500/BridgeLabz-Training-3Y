b63c95ae83decc81f7bc4b50fcdabdf16d30a46a
Himanshu Choudhary <chaudharyhimanshu276@gmail.com>
2025-08-13 22:19:47 +0530

    add problem

---
+++ fundamental-prectice-problem/PerimeterofRectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PerimeterofRectangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter length : ");
+        int l=sc.nextInt();
+        System.out.print("enter width : ");
+        int w=sc.nextInt();
+        int p=2*(l+w);
+        System.out.println("perimeter of rectangle : "+p);
+    }
+    
+}
