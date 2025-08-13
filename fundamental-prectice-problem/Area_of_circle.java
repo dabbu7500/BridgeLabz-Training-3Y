47e2fbe1e16af35bf82b6f73962ba94674a470a9
Himanshu Choudhary <chaudharyhimanshu276@gmail.com>
2025-08-13 12:39:50 +0530

    add problem 4

---
+++ fundamental-prectice-problem/Area_of_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Area_of_circle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter radius :");
+        float r=sc.nextFloat();
+        float area =(float) 3.14*r*r;
+        System.out.println("area of circle :"+area);
+
+    }
+}
